a9d01e3136214e8c28d2c41d49183c449c514082
choromba <horombacollin@gmail.com>
2023-03-29 19:00:15 +0200

    first attempt

---
+++ script.js
@@ -0,0 +1,17 @@
+let d = new Date();
+
+let newUpdate = document.getElementById('footer-sect');
+newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
+newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
+
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)
