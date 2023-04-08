59da0e5746a4890534f00a6565f5f6dd39a53944
choromba <horombacollin@gmail.com>
2023-04-08 10:23:38 +0200

    committed today

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function navToggle() {
 }
 
 btn.addEventListener('click', navToggle)
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
